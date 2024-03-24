1a8b51bab9d96d1d1ee464a20b10aa25a342697c
stereob1tch <nikita.dolb@yandex.ru>
2024-03-24 20:27:58 +0300

    123

--- Organiser/main.cpp
+++ Organiser/main.cpp
@@ -2,18 +2,6 @@
 
 #include <QApplication>
 
-#include <QtCharts/QChartView>
-#include <QtCharts/QBarSeries>
-#include <QtCharts/QBarSet>
-#include <QtCharts/QLegend>
-#include <QtCharts/QBarCategoryAxis>
-#include <QtCharts/QHorizontalStackedBarSeries>
-#include <QtCharts/QLineSeries>
-#include <QtCharts/QCategoryAxis>
-#include <QtCharts/QPieSeries>
-#include <QtCharts/QPieSlice>
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
